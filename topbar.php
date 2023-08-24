7ffd16e9709f7b7f9933b4ee90fac3e78f6b2b59
Suraj Singh <95633860+SinghSuraj-04092002@users.noreply.github.com>
2023-08-25 00:22:51 +0530

    Update topbar.php

--- topbar.php
+++ topbar.php
@@ -1,5 +1,5 @@
 <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-primary navbar-dark ">
+  <nav class="main-header navbar navbar-expand navbar-dark ">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
